61e9b78bdedc70e640f52edf107d477ddb6294b2
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2026-04-29 12:13:36 +0600

    protected-route

--- src/router/routes/ProtectRoute.js
+++ src/router/routes/ProtectRoute.js
@@ -1,26 +1,31 @@
- import React, { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-const ProtectRoute = ({route,children}) => {
-    const {role, userInfo} = useSelector(state => state.auth)
-    if (role) {
-        if (userInfo) {
-        if (userInfo.role === route.role) {
-            return <Suspense fallback={<div>Loading...</div>} >{children}</Suspense>
-        } else {
-            return <Navigate to='/unauthorized' replace />
-        }
-        
-       }
-        
-    }else {
-        return <Navigate to='/login' replace />
-    }
+const ProtectRoute = ({ route, children }) => {
+  const { role, userInfo, loader } = useSelector(state => state.auth);
 
 
+  if (loader) {
+    return <div>Loading...</div>;
+  }
 
-    
+
+  if (!userInfo) {
+    return <Navigate to="/login" replace />;
+  }
+
+ 
+  if (userInfo.role !== route.role) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </Suspense>
+  );
 };
 
 export default ProtectRoute;
