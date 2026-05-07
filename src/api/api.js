f953887067509bc34b449db12c20d0b25f9165a1
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2026-05-07 16:01:36 +0600

    backend-url-changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios"
 const api = axios.create({
-    baseURL: 'https://backend-multi-vendor-ecommerce-xa4b.onrender.com/api',
+    baseURL: 'https://multi-vendor-eccomerce-backend-1.onrender.com/api',
     withCredentials:true
 })
 export default api
 
-//https://backend-multi-vendor-ecommerce-xa4b.onrender.com
